933a2ae55953fcdd13eca49c1f59d23a6707c610
josiahbrooks21 <35280381+josiahbrooks21@users.noreply.github.com>
2018-09-05 21:49:21 -0400

    Create hello_world2.java
    I finished.

---
+++ hello_world2.java
@@ -0,0 +1,12 @@
+/*
+	Who: Josiah Brooks
+	When: Aug 28 2018
+	What: a1 hello world 
+*/
+class hellow {
+	// A java application will always have a main().
+	public static void main (String arg[]) {
+		System.out.println("Super excited for the year!");
+		System.out.println("Thanks for have me!");
+	}
+}
