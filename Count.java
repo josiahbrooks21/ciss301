7ce51802a463181fc313263198d846281c6df572
josiahbrooks21 <35280381+josiahbrooks21@users.noreply.github.com>
2018-12-03 22:58:06 -0500

    Create Count.java

---
+++ Count.java
@@ -0,0 +1,20 @@
+/*
+	Josiah Brooks
+	10/7/2018
+	Counting a6
+*/
+
+class Count {
+	
+	public static void main(String args[]) {
+			int count = 9;
+			
+			for(int i = count; i >= 1; --i) {
+				for( int j = 1; j <= i; ++j) {
+					System.out.print(j + " "); 
+				}
+			System.out.println();
+		
+	}
+}
+}
